bf074e1807976f5fa27c25b8840deb0ffe037974
jinglebiscuits <scott.wehby@gmail.com>
2015-07-16 21:22:12 -0500

    added sound and simple player controls. Not complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+    public float tilt;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = movement * speed;
+
+        rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	}
+}
